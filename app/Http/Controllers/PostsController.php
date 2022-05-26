7d3ec7913748bf30c4080130677893c111e994da
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-05-26 16:45:42 +0000

    Add post controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,10 +8,17 @@ use Illuminate\Support\Facades\DB;
 class PostsController extends Controller
 {
     function index(){
-        $id = 4;
-        $posts = DB::table('posts')
-            ->oldest()
-            ->get();
+        // normal query
+        // $posts = DB::select('select * from posts where id = :id', ['id' => 3]);
+        $posts = DB::select('select * from posts');
+
+        // query builder
+        // $id = 4;
+        // $posts = DB::table('posts')
+        //     ->where('id', $id)
+        //     ->update([
+        //         'title' => 'Updated post'
+        //     ]);
         dd($posts);
     }
 }
